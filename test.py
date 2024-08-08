0680aca79f89b9db4cc9b0e307062eadb1f0a582
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2024-08-08 20:16:33 +0700

    update

--- test.py
+++ test.py
@@ -1,21 +1,23 @@
 import heapq
 import math
 
+
 class Employee:
     def __init__(self, id, name, availability) -> None:
         self.id = id
         self.name = name
         self.availability = availability
-        self.shifts_assigned = 0
+        self.workload = 0
 
     # compare employees
     def __lt__(self, other):
-        if self.shifts_assigned == other.shifts_assigned:
+        if self.workload == other.workload:
             return len(self.availability) < len(other.availability)
-        return self.shifts_assigned < other.shifts_assigned
+        return self.workload < other.workload
     
-    def set_shifts_assigned(self, shifts_assigned):
-        self.shifts_assigned = shifts_assigned
+    def set_workload(self, workload):
+        self.workload = workload
+
 
 class PriorityQueue:
     def __init__(self, iterable) -> None:
@@ -31,25 +33,29 @@ class PriorityQueue:
     def is_empty(self):
         return len(self.queue) == 0
 
+
 class Scheduler:
-    def __init__(self, availability, staff, shift_labels) -> None:
-        self.num_people = len(staff)
-        self.num_days = 7
+    def __init__(self, availability, employee_names, day_labels, shift_labels) -> None:
+        self.day_labels = day_labels
+        self.shift_labels = shift_labels
+        self.num_employees = len(employee_names)
+        self.num_days = len(day_labels)
         self.num_shifts = len(shift_labels)
-        self.DAY_LABELS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-        self.SHIFT_LABELS = shift_labels
-        
-        self.employees = [Employee(i, staff[i], availability[i]) for i in range(self.num_people)]
+
+        self.employees = [Employee(i, employee_names[i], availability[i]) for i in range(self.num_employees)]
 
         self.shift_availability = self.create_shift_availability(availability)
 
+        self.WORKLOAD_LIMIT_PER_PERSON = self.calculate_workload_limit()
+
         self.schedule = [[-1 for _ in range(self.num_shifts)] for _ in range(self.num_days)]
 
-        self.SHIFT_LIMIT_PER_PERSON  = math.ceil(self.num_days * self.num_shifts / self.num_people)
-    
-    def get_employee_by_id(self, id):
-        return self.employees[id]
-    
+
+    def calculate_workload_limit(self):
+        # TODO: workload limit can be adjusted here
+        return math.ceil(self.num_days * self.num_shifts / self.num_employees)
+
+
     def create_shift_availability(self, availability):
         # shift_availability[day][shift] = list of people available for that shift in that day
         shift_availability = [[[] for _ in range(self.num_shifts)] for _ in range(self.num_days)]
@@ -57,11 +63,13 @@ class Scheduler:
             # list of shifts that person is available for each day
             for day in range(self.num_days):
                 for shift in person_availability[day]:
-                    shift_availability[day][shift].append(self.get_employee_by_id(person_id))
+                    shift_availability[day][shift].append(self.employees[person_id])
         return shift_availability
 
-    def valid(self, employee, day, shift):
-        return employee.id not in self.schedule[day] and employee.shifts_assigned < self.SHIFT_LIMIT_PER_PERSON
+
+    def can_assign(self, employee, day, shift):
+        # MORE CONDITIONS CAN BE ADDED HERE
+        return employee.id not in self.schedule[day] and employee.workload < self.WORKLOAD_LIMIT_PER_PERSON
 
 
     def prioritize_shifts(self, day):
@@ -70,44 +78,40 @@ class Scheduler:
         return shifts
 
 
-    def assign_shifts(self):
+    def assign_work(self):
         for day in range(self.num_days):
             shifts = self.prioritize_shifts(day)
             for shift in shifts:
                 prioritized_people = PriorityQueue(self.shift_availability[day][shift])
                 while not prioritized_people.is_empty() and self.schedule[day][shift] == -1:
                     employee = prioritized_people.pop()
-                    if self.valid(employee, day, shift):
+                    if self.can_assign(employee, day, shift):
                         self.schedule[day][shift] = employee.id
-                        employee.set_shifts_assigned(employee.shifts_assigned + 1)
+                        employee.set_workload(employee.workload + 1)
 
         return self.schedule
     
-    def day_to_string(self, day):
-        return self.DAY_LABELS[day]
-    
-    def shift_to_string(self, shift):
-        return self.SHIFT_LABELS[shift]
-    
+
     def print_schedule(self):
         header = "\t"
-        for day in self.DAY_LABELS:
+        for day in  self.day_labels:
             header += f"{day}\t"
         print(header)
         for shift in range(self.num_shifts):
-            row = f"{self.SHIFT_LABELS[shift]}\t"
+            row = f"{self.shift_labels[shift]}\t"
             for day in range(self.num_days):
                 if self.schedule[day][shift] == -1:
                     row += f"None\t"
                 else:
-                    employee = self.get_employee_by_id(self.schedule[day][shift])
+                    employee = self.employees[self.schedule[day][shift]]
                     row += f"{employee.name}\t"
             print(row)
 
 
 def main():
+    day_labels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     shift_labels = ["S", "T", "C"]
-    employees = ["Dat", "Danh", "Khoi", "Anh", "Tan", "Tien"]
+    employee_names = ["Dat", "Danh", "Khoi", "Anh", "Tan", "Tien"]
     availability = [
         [[0, 1], [0, 1], [], [1, 2], [0, 1, 2], [0, 2], [1, 2]],
         [[0, 2], [], [0, 1, 2], [0, 1], [1, 2], [0, 1], [0, 2]],
@@ -117,10 +121,10 @@ def main():
         [[2], [2], [1], [0, 1], [2], [0], []]
     ]
 
-    scheduler = Scheduler(availability, employees, shift_labels)
-    scheduler.assign_shifts()
+    scheduler = Scheduler(availability, employee_names, day_labels, shift_labels)
+    scheduler.assign_work()
     scheduler.print_schedule()
+
 
 if __name__ == "__main__":
     main()
-
